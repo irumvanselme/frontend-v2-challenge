0da204323562377912ec0ee9e19a2f1e72c64b44
irumvanselme <andesanselme@gmail.com>
2021-08-18 20:29:27 +0200

    Make the Search Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+
 import Footer from "./components/pages/homepage/Footer/Footer";
 import Profile from "./components/pages/homepage/Profile/Profile";
 import Component from "./components/pages/homepage/PerContinent/PerContinent";
+import Search from "./components/pages/homepage/Search/Search";
 
 function App() {
     return (
         <>
+            <Search />
             <Component />
             <Profile />
             <Footer />

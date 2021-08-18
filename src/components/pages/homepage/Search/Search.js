e12258ad55f054f356e58721ba548b51164dbcf7
irumvanselme <andesanselme@gmail.com>
2021-08-18 21:06:17 +0200

    Add an app title

--- src/components/pages/homepage/Search/Search.js
+++ src/components/pages/homepage/Search/Search.js
@@ -2,10 +2,19 @@ import React from "react";
 
 import "./Search.css"
 
-export default function Search(){
+export default function Search() {
     return (
-        <div>
-            dsdsds
+        <div className="_search-container pt-5">
+            <div className="space-horizontal d-flex justify-content-between">
+                <div className="_logo title">COVICALC</div>
+                <div>
+                    <button className="_button">CONTACT</button>
+                </div>
+            </div>
+            <div className="text-center mt-5">
+                <h1 style={{ fontSize: 50, color: "white" }} className="title">UPDATES</h1>
+                <div style={{ fontWeight: 300, color: "white" }}>Search a country</div>
+            </div>
         </div>
     )
 }

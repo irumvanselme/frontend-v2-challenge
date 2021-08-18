f64a22894581072f50f5f2c2a1d529db2406be91
irumvanselme <andesanselme@gmail.com>
2021-08-18 19:28:13 +0200

    Made the Components page with sliders

--- src/components/pages/homepage/PerContinent/Components/Components.js
+++ src/components/pages/homepage/PerContinent/Components/Components.js
@@ -1,22 +1,37 @@
 import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
 
 import "./Components.css"
 import 'swiper/swiper.min.css';
+import Component from "../Component/Component";
 
-export default function Components(){
+SwiperCore.use([ Navigation ]);
+
+export default function Components() {
     return (
         <div>
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 1</SwiperSlide>
+            <Swiper spaceBetween={50} slidesPerView={3} navigation={{ nextEl: ".prev-btn", prevEl: ".next-btn" }}>
+                <SwiperSlide>
+                    <Component/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Component/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Component/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Component/>
+                </SwiperSlide>
+                <div className="_navigation-buttons">
+                    <div className="_btn prev-btn d-flex justify-content-center align-items-center">
+                        <img src={"/svgs/ic-arrows-left.svg"} width={30} height={40}/>
+                    </div>
+                    <div className="_btn next-btn d-flex justify-content-center align-items-center">
+                        <img src={"/svgs/ic-arrows-left.svg"} width={30} height={40}/>
+                    </div>
+                </div>
             </Swiper>
         </div>
     )
--- src/components/pages/homepage/PerContinent/PerContinent.js
+++ src/components/pages/homepage/PerContinent/PerContinent.js
@@ -3,13 +3,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import "./PerContinent.css"
 import 'swiper/swiper.min.css';
+
 import Components from "./Components/Components";
 
 export default function PerContinent() {
     return (
         <div className="text-center">
             <h1 className="title _title-per-continents">PER CONTINENTS</h1>
-            <Components/>
+            <div className="_body-per-continent">
+                <Components/>
+            </div>
         </div>
     )
 }

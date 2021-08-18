cda96860731af11db775b1b30f83cd27b7be2a25
irumvanselme <andesanselme@gmail.com>
2021-08-18 14:32:07 +0200

    Add Footer to the Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Footer from "./components/pages/homepage/Footer/Footer";
 
 function App() {
     return (
-        <div className="App">
-            <h1 className="title">this is the app</h1>
-        </div>
+        <>
+            <Footer />
+        </>
     );
 }
 
